140befb6481de23ec8bbb5d41860c184b0f6f77e
Mike Hepburn <eformat@gmail.com>
2019-07-07 11:24:58 +1000

    apply config every sync, leave k8s module to ratify

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ docker login -u $(oc whoami) -p $(oc whoami -t) $HOST
 docker build -t gitops-ansible-operator -f build/Dockerfile .
 docker tag gitops-ansible-operator:latest ${HOST}/gitops-ansible-operator/gitops-ansible-operator:latest
 docker push ${HOST}/gitops-ansible-operator/gitops-ansible-operator:latest
+oc delete $(oc get pod -o name -l name=gitops-ansible-operator)
